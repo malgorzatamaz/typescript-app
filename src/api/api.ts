511c9c60a56882789ad70707bb2739442b9ff5fd
malgorzatamaz <m.mazurek@selleo.com>
2021-06-04 10:35:40 +0200

    fixed url to use https

--- src/api/api.ts
+++ src/api/api.ts
@@ -7,7 +7,7 @@ import {
   MovieResponse,
 } from "../types/movieTypes";
 
-const BASE_URL = `http://www.omdbapi.com/?apikey=${process.env.REACT_APP_API_KEY}`;
+const BASE_URL = `https://www.omdbapi.com/?apikey=${process.env.REACT_APP_API_KEY}`;
 
 interface ApiGetConfig {
   setLoading: Dispatch<SetStateAction<boolean>>;
